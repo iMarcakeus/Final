90bd53da3129b955921d65ace624ed7ba94f73f5
iMarcakeus <44680380+iMarcakeus@users.noreply.github.com>
2018-11-13 13:42:35 -0600

    Update final.cpp

--- final.cpp
+++ final.cpp
@@ -4,5 +4,39 @@
 #include <fstream>
 #include <cmath>
 #include <ctime>
+#include <cstdlib>
 using namespace std;
 
+void mainMenu();
+
+int main(){
+    int gameState = 0;
+    while (true){
+        if (gameState == 0){
+            mainMenu();
+        }
+    }
+}
+
+void mainMenu(){
+    string command;
+    cout <<"      #######                                                                  " << endl;
+    cout <<"    /       ###                                      #                         " << endl;
+    cout <<"   /         ##                                     ###                        " << endl;
+    cout <<"   ##        #                          ##           #  ##                     " << endl;
+    cout <<"    ###                                 ##              ##                     " << endl;
+    cout <<"   ## ###       ##   ####    ###  /###   ##    ### ###   ##    ###      /##    " << endl;
+    cout <<"    ### ###      ##    ###  / ###/ #### / ##    ### ###   ##    ###    / ###   " << endl;
+    cout <<"      ### ###    ##     ###/   ##   ###/  ##     ### ##   ##     ###  /   ###  " << endl;
+    cout <<"        ### /##  ##      ##    ##         ##      ## ##   ##      ## ##    ### " << endl;
+    cout <<"          #/ /## ##      ##    ##         ##      ## ##   ##      ## ########  " << endl;
+    cout <<"           #/ ## ##      ##    ##         ##      ## ##   ##      ## #######   " << endl;
+    cout <<"            # /  ##      ##    ##         ##      ## ##   ##      ## ##        " << endl;
+    cout <<"  /##        /   ##      /#    ##         ##      /  ##   ##      /  ####    / " << endl;
+    cout <<" /  ########/     ######/ ##   ###         ######/   ### / ######/    ######/  " << endl;
+    cout <<"/     #####        #####   ##   ###         #####     ##/   #####      #####   " << endl;
+    cout <<"|                                                                              " << endl;
+    cout <<" )                                                                            " << endl;
+    cout <<"NEW GAME       LOAD GAME        OPTIONS         HELP          EXIT" << endl;
+    getline(cin, command);
+}
